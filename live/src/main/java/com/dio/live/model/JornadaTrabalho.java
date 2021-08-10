7672bb259d0c085afce556c9c85f2bf4f9215466
Ruan Bomfim <ruanmarcos97@hotmail.com>
2021-08-09 21:56:33 -0300

    Add files via upload

---
+++ live/src/main/java/com/dio/live/model/JornadaTrabalho.java
@@ -0,0 +1,23 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class JornadaTrabalho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String descricao;
+}
